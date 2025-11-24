408e262ff453eed05279924a9dd35b340e003a08
FleetyCodes <laszlo.murzsicz94@gmail.com>
2025-11-24 23:48:20 +0100

    cors update

--- backend/src/main/java/com/comicvaultraiders/comicvaultraiders/security/WebSecurityConfig.java
+++ backend/src/main/java/com/comicvaultraiders/comicvaultraiders/security/WebSecurityConfig.java
@@ -72,7 +72,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://comicvaultraiders.eu","https://www.comicvaultraiders.eu"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
